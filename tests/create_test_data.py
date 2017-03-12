dc4e0c815d71519f995093606bef6d0c23a631b3
Boris Paschenko <i_hope-you_die@mail.ru>
2017-03-12 22:58:04 +0300

    add test data

---
+++ tests/create_test_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+names = ['Bob', 'Jessica', 'Mary', 'John', 'Mel']
+age = [18, 40, 34, 22, 57]
+work_start = [7, 10, 11, 8, 11]
+work_end = [11, 14, 12, 17, 15]
+experience = [0, 10, 3, 2, 20]
+home = [[0.123123, 5.12312], [0.123123, 5.12312], [0.123123, 5.12312],
+        [0.123123, 5.12312], [0.123123, 5.12312]]
+work = [[6.123123, 6.12312], [6.123123, 6.12312], [6.123123, 6.12312],
+        [6.123123, 6.12312], [6.123123, 6.12312]]
+
+DriverDataSet = list(zip(names,age,work_start,work_end,experience,home,work))
+
+df = pd.DataFrame(data = DriverDataSet, columns = ['Names', 'Age', 'Work Start',
+                                                   'Work End', 'Experience',
+                                                   'Home', 'Work'])
+
+df.to_csv('Data/drivers.csv')
